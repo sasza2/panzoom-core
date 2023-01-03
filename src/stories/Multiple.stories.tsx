173481269867e15384aa775ad1ca94e1497fc6d7
sasza <sasza@onet.eu>
2023-01-03 02:35:17 +0100

    styles for container

--- src/stories/Multiple.stories.tsx
+++ src/stories/Multiple.stories.tsx
@@ -6,7 +6,7 @@ import initPanZoom from '@/initPanZoom';
 
 export default { title: 'Multiple' };
 
-const ELEMENTS = 50;
+const ELEMENTS = 30;
 
 export const Multiple = () => {
   const [initialized, setInitialized] = useState<boolean>(false);
@@ -20,13 +20,17 @@ export const Multiple = () => {
   useEffect(() => {
     if (initialized) {
       panZoomRef.current = initPanZoom(childNode.current);
+      const cx = 120;
+      const cy = 120;
+      const r = 100;
       for (let i = 0; i < ELEMENTS; i++) {
+        const a = i * (360 / ELEMENTS) * (Math.PI / 180);
         panZoomRef.current.addElement(
           document.querySelector(`[data-id="element-${i}"]`),
           {
             id: i.toString(),
-            x: i * 10,
-            y: i * 10,
+            x: cx + r * Math.sin(a),
+            y: cy + r * Math.cos(a),
             family: `family-${Math.floor(i / 5)}`,
           },
         );
@@ -58,7 +62,7 @@ export const Multiple = () => {
             </button>
           )
       }
-      <div>
+      <div style={{ minHeight: 300 }}>
         <div ref={childNode}>
           {renderElements()}
         </div>
