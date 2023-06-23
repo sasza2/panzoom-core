a92696130d8b167e3fc884b2a16788928c436648
sasza <sasza@onet.eu>
2023-06-23 15:23:29 +0200

    api - silent update element position

--- src/api/elements.ts
+++ src/api/elements.ts
@@ -15,19 +15,45 @@ type UpdateElementPosition = ({
   onElementsChangeRef: Ref<OnElementsChange>,
 }) => (id: string | number, position: Position) => void;
 
-export const updateElementPosition: UpdateElementPosition = ({
+const update = ({
+  id,
   elementsRef,
-  onElementsChangeRef,
-}) => (id, position) => {
+  position,
+}: {
+  id: string | number,
+  elementsRef: Elements,
+  position: Position,
+}) => {
   const element = elementsRef.current[id as string];
   if (!element) return;
 
   element.node.current.style.transform = produceStyle({ position });
   element.position = position;
+};
+
+export const updateElementPosition: UpdateElementPosition = ({
+  elementsRef,
+  onElementsChangeRef,
+}) => (id, position) => {
+  update({
+    id,
+    elementsRef,
+    position,
+  });
 
   if (onElementsChangeRef.current) {
     onElementsChangeRef.current({
-      [element.id]: position,
+      [id]: position,
     });
   }
+};
+
+export const updateElementPositionSilent: UpdateElementPosition = ({
+  elementsRef,
+}) => (id, position) => {
+  update({
+    id,
+    elementsRef,
+    position,
+  });
 };
